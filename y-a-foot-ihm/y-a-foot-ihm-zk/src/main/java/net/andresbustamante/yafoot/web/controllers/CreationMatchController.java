445f454a3e380d7e3f9dd303ee9fc3cf4e2e5a57
Andrés Bustamante <afbustag@gmail.com>
2019-02-28 14:16:47 +0100

    Avancée sur le formulaire de création des matchs

--- y-a-foot-ihm/y-a-foot-ihm-zk/src/main/java/net/andresbustamante/yafoot/web/controllers/CreationMatchController.java
+++ y-a-foot-ihm/y-a-foot-ihm-zk/src/main/java/net/andresbustamante/yafoot/web/controllers/CreationMatchController.java
@@ -1,9 +1,14 @@
 package net.andresbustamante.yafoot.web.controllers;
 
+import net.andresbustamante.yafoot.model.xs.Site;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
+import org.zkoss.zul.*;
+
+import java.util.ArrayList;
 
 /**
  * @author andresbustamante
@@ -12,4 +17,25 @@ import org.zkoss.zkplus.spring.DelegatingVariableResolver;
 public class CreationMatchController extends SelectorComposer<Component> {
 
     private static final long serialVersionUID = 1L;
+
+    @Wire
+    private Datebox txtDateMatch;
+
+    @Wire
+    private Spinner txtNbMinJoueurs;
+
+    @Wire
+    private Spinner txtNbMaxJoueurs;
+
+    @Wire
+    private Combobox cbbSite;
+
+    private ListModel<Site> sitesModel = new ListModelArray<>(new ArrayList<>());
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+
+        cbbSite.setModel(sitesModel);
+    }
 }
