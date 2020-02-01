be69d78c9e0e1d6fef3cbf52d496daf1e17aed4a
Andrés Bustamante <afbustag@gmail.com>
2020-02-01 19:50:18 +0100

    Fixed problem for POST calls to backend controllers

--- y-a-foot-core/src/main/java/net/andresbustamante/yafoot/config/WebSecurityConfig.java
+++ y-a-foot-core/src/main/java/net/andresbustamante/yafoot/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
                 .anyRequest().authenticated()
