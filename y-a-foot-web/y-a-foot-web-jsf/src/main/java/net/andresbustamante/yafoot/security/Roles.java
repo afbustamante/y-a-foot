48c886b295f3bd175157d6139ebd16d19f98a588
Andrés Bustamante <afbustag@gmail.com>
2020-03-07 13:01:33 +0100

    JSF module deleted

--- y-a-foot-web/y-a-foot-web-jsf/src/main/java/net/andresbustamante/yafoot/security/Roles.java
+++
@@ -1,9 +0,0 @@
-package net.andresbustamante.yafoot.security;
-
-public class Roles {
-
-    public static final String ADMINISTRATOR = "ADMINISTRATOR";
-    public static final String PLAYER = "PLAYER";
-
-    private Roles() {}
-}
