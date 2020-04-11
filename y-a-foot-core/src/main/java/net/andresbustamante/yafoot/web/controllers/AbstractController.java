8a9d0acfe55f5835758f5879a2cbe4bc0ca53a5a
Andrés Bustamante <afbustag@gmail.com>
2020-04-11 12:30:26 +0200

    Fix for correctly recovering Spring Security's authenticated username

--- y-a-foot-core/src/main/java/net/andresbustamante/yafoot/web/controllers/AbstractController.java
+++ y-a-foot-core/src/main/java/net/andresbustamante/yafoot/web/controllers/AbstractController.java
@@ -4,17 +4,18 @@ import net.andresbustamante.yafoot.exceptions.ApplicationException;
 import net.andresbustamante.yafoot.model.UserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.ZoneId;
 
-import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
-
 /**
+ * Abstract controller for common RESTful controllers operations
+ *
  * @author andresbustamante
  */
 public abstract class AbstractController {
@@ -25,22 +26,24 @@ public abstract class AbstractController {
         try {
             return new URI(location);
         } catch (URISyntaxException e) {
-            log.error("Erreur lors de la construction de la location d'une ressource", e);
+            log.error("An error occurred while building a location URI for a new resource", e);
             return null;
         }
     }
 
     protected UserContext getUserContext(HttpServletRequest request) throws ApplicationException {
-        SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute(SPRING_SECURITY_CONTEXT_KEY);
-        UserDetails userDetails = (UserDetails) securityContext.getAuthentication().getPrincipal();
-        String username = userDetails.getUsername();
-        String timeZone = request.getHeader(UserContext.TZ);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (username != null) {
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            // User successfully authenticated
+            String username = authentication.getName();
+            String timeZone = request.getHeader(UserContext.TZ);
+
             UserContext userContext = new UserContext(username);
             userContext.setTimezone(timeZone != null ? ZoneId.of(timeZone) : ZoneId.systemDefault());
             return userContext;
         } else {
+            // Anonymous user
             throw new ApplicationException("Unable to find a username");
         }
     }
