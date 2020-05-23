70c2d8d5f2ec0e4d08b39a578449efcb7b83cbd8
Andrés Bustamante <afbustag@gmail.com>
2020-05-23 19:34:42 +0200

    Added support for Spring LDAP testing under UnboundID

--- y-a-foot-services/src/main/java/net/andresbustamante/yafoot/config/LdapConfig.java
+++ y-a-foot-services/src/main/java/net/andresbustamante/yafoot/config/LdapConfig.java
@@ -1,6 +1,5 @@
 package net.andresbustamante.yafoot.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
@@ -9,26 +8,8 @@ import org.springframework.ldap.core.support.LdapContextSource;
 @Configuration
 public class LdapConfig {
 
-    @Value("${spring.ldap.urls}")
-    private String url;
-
-    @Value("${spring.ldap.username}")
-    private String userDn;
-
-    @Value("${spring.ldap.password}")
-    private String password;
-
     @Bean
-    public LdapContextSource contextSource() {
-        LdapContextSource contextSource= new LdapContextSource();
-        contextSource.setUrl(url);
-        contextSource.setUserDn(userDn);
-        contextSource.setPassword(password);
-        return contextSource;
-    }
-
-    @Bean
-    public LdapTemplate ldapTemplate() {
-        return new LdapTemplate(contextSource());
+    public LdapTemplate ldapTemplate(LdapContextSource contextSource) {
+        return new LdapTemplate(contextSource);
     }
 }
--- y-a-foot-services/src/test/java/net/andresbustamante/yafoot/config/LdapTestConfig.java
+++ y-a-foot-services/src/test/java/net/andresbustamante/yafoot/config/LdapTestConfig.java
@@ -1,11 +1,55 @@
 package net.andresbustamante.yafoot.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.test.unboundid.EmbeddedLdapServerFactoryBean;
+import org.springframework.ldap.test.unboundid.LdifPopulator;
 
 @Configuration
 @ComponentScan(basePackages = {"net.andresbustamante.yafoot.ldap"})
 @PropertySource("classpath:application.properties")
 public class LdapTestConfig {
+
+    @Value("${ldap.config.url}")
+    private String url;
+
+    @Value("${ldap.config.username}")
+    private String userDn;
+
+    @Value("${ldap.config.password}")
+    private String password;
+
+    @Bean
+    public EmbeddedLdapServerFactoryBean embeddedLdapServer() {
+        EmbeddedLdapServerFactoryBean ldapServerFactoryBean = new EmbeddedLdapServerFactoryBean();
+        ldapServerFactoryBean.setPartitionName("test");
+        ldapServerFactoryBean.setPartitionSuffix("ou=test,dc=andresbustamante,dc=net");
+        ldapServerFactoryBean.setPort(6389);
+        return ldapServerFactoryBean;
+    }
+
+    @Bean
+    public LdifPopulator ldifPopulator() {
+        LdifPopulator ldifPopulator = new LdifPopulator();
+        ldifPopulator.setContextSource(contextSource());
+        ldifPopulator.setResource(new ClassPathResource("data.ldif"));
+        ldifPopulator.setBase("ou=test,dc=andresbustamante,dc=net");
+        ldifPopulator.setClean(true);
+        ldifPopulator.setDefaultBase("ou=test,dc=andresbustamante,dc=net");
+        return ldifPopulator;
+    }
+
+    @Bean
+    public LdapContextSource contextSource() {
+        LdapContextSource contextSource= new LdapContextSource();
+        contextSource.setUrl(url);
+        contextSource.setUserDn(userDn);
+        contextSource.setPassword(password);
+        return contextSource;
+    }
 }
