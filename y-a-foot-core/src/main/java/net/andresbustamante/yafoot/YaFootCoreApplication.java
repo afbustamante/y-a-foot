57e6af3746fd0bb1343a324409c49b63b54822eb
Andrés Bustamante <afbustag@gmail.com>
2020-06-07 20:15:15 +0200

    Backend transformed to WAR deployment (optimised for Wildfly 16)

--- y-a-foot-core/src/main/java/net/andresbustamante/yafoot/YaFootCoreApplication.java
+++ y-a-foot-core/src/main/java/net/andresbustamante/yafoot/YaFootCoreApplication.java
@@ -2,13 +2,20 @@ package net.andresbustamante.yafoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = "net.andresbustamante.yafoot")
 @EnableAspectJAutoProxy
-public class YaFootCoreApplication {
+public class YaFootCoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(YaFootCoreApplication.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(YaFootCoreApplication.class);
     }
 }
